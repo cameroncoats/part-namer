9072b0b8f6e970b5ee0d800c0d495ecab533a62d
Cameron Coats <c@meroncoats.co.uk>
2022-05-28 16:32:47 +0300

    add GetPartCategories method to PartCategoryService

--- src/app/part-category.service.ts
+++ src/app/part-category.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 
+import { PartCategory, PartCategoryFieldType } from "../PartCategory";
+import { PART_CATEGORIES } from '../PART_CATEGORIES';
+
 @Injectable({
   providedIn: 'root'
 })
 export class PartCategoryService {
 
   constructor() { }
+
+  getPartCategories(){
+    return PART_CATEGORIES;
+  }
 }
